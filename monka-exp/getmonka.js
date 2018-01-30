665e2e99f9d4ebf72ac68c20f0b890d0efbae9b8
londes <william@chartbeat.com>
2018-01-29 22:15:05 -0500

    monkaS all errywhur

--- monka-exp/getmonka.js
+++ monka-exp/getmonka.js
@@ -3,27 +3,26 @@ var monkaCanvas=document.createElement("canvas");
 monkaCanvas.id="monka-canvas";
 monkaCanvas.height=window.innerHeight;
 monkaCanvas.width=window.innerWidth;
-var ctx = monkaCanvas.getContext("2d");
+ctx = monkaCanvas.getContext("2d");
 
 // creates X monka image elements of random size and appends them to the canvas element
 
-var amountOfMonkaS = 100;
+var amountOfMonkaS = 20;
 for (i=0; i<amountOfMonkaS; i++) {
   var whichMonka = 0;
   var monkaS=document.createElement("img");
   monkaS.id="monka-" + whichMonka + "";
   monkaS.src="/img/TrymOlU.jpg";
   monkaS.alt="shheeeeeeee";
-  randHeight=;
-  randWidth=;
-  monkaS.height="84";
-  monkaS.width="152";
-  ctx.drawImage(monkaS,0,0,monkaS.width,monkaS.height);
+  //randHeight= "" + 1080*Math.random() + "";
+  //randWidth= "" + 1920*Math.random() + "";
+  randXPos= window.innerWidth*Math.random();
+  randYPos= window.innerHeight*Math.random();
+  monkaS.height=""+ 168*Math.random()+ "";
+  monkaS.width="" + 304*Math.random()+ "";
+  ctx.drawImage(monkaS,randXPos,randYPos,monkaS.width,monkaS.height);
   whichMonka++;
 }
-
-
-
 
 document.body.appendChild(monkaCanvas);
 document.body.appendChild(monkaS);
