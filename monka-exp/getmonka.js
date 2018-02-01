4cc8e432fd669676c91deddb4430986fc0c8a8e6
londes <william@chartbeat.com>
2018-02-01 13:44:00 -0500

    wut happen m8 no more monkaS feelsbad

--- monka-exp/getmonka.js
+++ monka-exp/getmonka.js
@@ -3,27 +3,46 @@ var monkaCanvas=document.createElement("canvas");
 monkaCanvas.id="monka-canvas";
 monkaCanvas.height=window.innerHeight;
 monkaCanvas.width=window.innerWidth;
-ctx = monkaCanvas.getContext("2d");
+var ctx = monkaCanvas.getContext("2d");
 
-// creates X monka image elements of random size and appends them to the canvas element
+var amountOfMonkaS = 4;
+monkaDrawer(ctx, amountOfMonkaS);
 
-var amountOfMonkaS = 20;
-for (i=0; i<amountOfMonkaS; i++) {
-  var whichMonka = 0;
-  var monkaS=document.createElement("img");
-  monkaS.id="monka-" + whichMonka + "";
-  monkaS.src="/img/TrymOlU.jpg";
-  monkaS.alt="shheeeeeeee";
-  //randHeight= "" + 1080*Math.random() + "";
-  //randWidth= "" + 1920*Math.random() + "";
-  randXPos= window.innerWidth*Math.random();
-  randYPos= window.innerHeight*Math.random();
-  monkaS.height=""+ 168*Math.random()+ "";
-  monkaS.width="" + 304*Math.random()+ "";
-  ctx.drawImage(monkaS,randXPos,randYPos,monkaS.width,monkaS.height);
-  whichMonka++;
+function getOnloadFunction(img, xpos, ypos) {
+  return function drawInContext() {
+    console.log("xpos: " + xpos +", canvas width: " + monkaCanvas.width + "");
+    console.log("ypox: " + ypos +", canvas height: " + monkaCanvas.height + "");
+    console.log(img);
+    console.log("---------------------------");
+    ctx.drawImage(img, xpos, ypos);
+  }
 }
 
-document.body.appendChild(monkaCanvas);
-document.body.appendChild(monkaS);
-whichMonka++;
+function monkaDrawer(ctx,amountOfMonkaS) {
+  var whichMonka = 0;
+
+  for (i=0; i<amountOfMonkaS; i++) {
+    var monkaS=document.createElement("img");
+    monkaS.id="monka-" + whichMonka + "";
+    monkaS.alt="shheeeeeeee";
+    // monkaS.height=""+ 168*Math.random() + "";
+    // monkaS.width="" + 304*Math.random() + "";
+    monkaS.height="168";
+    monkaS.width="304";
+
+    randXPos= window.innerWidth*Math.random();
+    randYPos= window.innerHeight*Math.random();
+
+    monkaS.onload = getOnloadFunction(monkaS,10,10);
+    monkaS.src="/img/TrymOlU.jpg";
+    whichMonka++;
+    // monkaS.height="168";
+    // monkaS.width="304";
+  }
+}
+
+window.onload = function() {
+  document.body.appendChild(monkaCanvas);
+}
+
+// creates amountOfMonkaS monka image elements of random size and appends them to the canvas element
