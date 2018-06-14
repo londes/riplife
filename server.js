aa9e9ca778f69d6cac108c1861cc7b6b972e447b
William Purcell <william@chartbeat.com>
2018-06-14 12:58:14 -0400

    further changes to server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(function(req, res, next) {
 //now we can set the route path & initialize the API
 router.get('/', function(req, res) {
  //res.json({ message: 'API Initialized'}); // send index.html
- res.sendFile('./../build/index.html');
+ res.sendFile('./build/index.html');
 });
 
 //adding the /videos route to our /api router
