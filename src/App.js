631896efdc68b53e95a83df37d91b6461879656b
William Purcell <william@chartbeat.com>
2018-09-22 13:57:14 -0400

    added favicon

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     fetch('https://rippy-app.herokuapp.com/last5rips')
       .then(response => {
         return response.json();
-      })
+      })  
       .then(responseJson => {
         console.log(responseJson);
         this.setState({
